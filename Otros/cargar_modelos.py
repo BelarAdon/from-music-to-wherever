5a456b29efbc4fbfa8890e610a7870ee25307837
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-11 20:21:30 +0200

    Update cargar_modelos.py

--- Otros/cargar_modelos.py
+++ Otros/cargar_modelos.py
@@ -7,16 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from huggingface_hub import hf_hub_download
 import joblib
 
-repo_id = "TU_USUARIO/tu-repo-modelos"
+repo_id = "Eskarcho/modelo_streamlit"
 
 scaler = joblib.load(hf_hub_download(repo_id, "modelos/scaler.pkl"))
 umap_model = joblib.load(hf_hub_download(repo_id, "modelos/umap_model.pkl"))
 kmeans = joblib.load(hf_hub_download(repo_id, "modelos/kmeans_umap.pkl"))
 feature_cols = joblib.load(hf_hub_download(repo_id, "modelos/feature_cols.pkl"))
-
-scaler = joblib.load(MODELS_DIR / "scaler.pkl")
-umap_model = joblib.load(MODELS_DIR / "umap_model.pkl")
-kmeans = joblib.load(MODELS_DIR / "kmeans_umap.pkl")
-feature_cols = joblib.load(MODELS_DIR / "feature_cols.pkl")
 
     
