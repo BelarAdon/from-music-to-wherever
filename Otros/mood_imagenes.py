fc4e5251d83803723d5a9f5d46b32ca47d52b101
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-11 19:41:42 +0200

    Delete Otros directory

--- Otros/mood_imagenes.py
+++
@@ -1,9 +0,0 @@
-mood_imagenes = {
-    "Concierto / Euforia": "imagenes/euforia.jpg",
-    "Intenso / Dramático": "imagenes/dramatico.jpg",
-    "Instrumental / Concentración": "imagenes/concentracion.jpg",
-    "Chill groove / Urbano suave": "imagenes/chillgroove.jpg",
-    "Happy / Buen rollo": "imagenes/happy.jpg",
-    "Fiesta / Subidón": "imagenes/fiesta.jpg",
-    "Triste / Melancólico": "imagenes/melanciolia.jpg"
-}
