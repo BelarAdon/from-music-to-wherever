460fdb9b2cd560d40e36da61d75bb5ccc437a322
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-11 20:20:40 +0200

    Update cargar_modelos.py

--- Otros/cargar_modelos.py
+++ Otros/cargar_modelos.py
@@ -3,7 +3,16 @@ import json
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-MODELS_DIR = BASE_DIR / "modelos" 
+
+from huggingface_hub import hf_hub_download
+import joblib
+
+repo_id = "TU_USUARIO/tu-repo-modelos"
+
+scaler = joblib.load(hf_hub_download(repo_id, "modelos/scaler.pkl"))
+umap_model = joblib.load(hf_hub_download(repo_id, "modelos/umap_model.pkl"))
+kmeans = joblib.load(hf_hub_download(repo_id, "modelos/kmeans_umap.pkl"))
+feature_cols = joblib.load(hf_hub_download(repo_id, "modelos/feature_cols.pkl"))
 
 scaler = joblib.load(MODELS_DIR / "scaler.pkl")
 umap_model = joblib.load(MODELS_DIR / "umap_model.pkl")
