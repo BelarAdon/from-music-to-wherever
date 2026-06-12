57c1cb182a1ceba720238a51be86dd2372bf16b2
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-12 09:46:57 +0200

    Update cargar_modelos.py

--- Otros/cargar_modelos.py
+++ Otros/cargar_modelos.py
@@ -1,17 +1,36 @@
 import joblib
-import json
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 from huggingface_hub import hf_hub_download
-import joblib
 
 repo_id = "Eskarcho/modelo_streamlit"
 
-scaler = joblib.load(hf_hub_download(repo_id, "modelos/scaler.pkl"))
-umap_model = joblib.load(hf_hub_download(repo_id, "modelos/umap_model.pkl"))
-kmeans = joblib.load(hf_hub_download(repo_id, "modelos/kmeans_umap.pkl"))
-feature_cols = joblib.load(hf_hub_download(repo_id, "modelos/feature_cols.pkl"))
+# Variables globales (no cargadas aún)
+scaler = None
+kmeans = None
+umap_model = None
+feature_cols = None
 
-    
+
+def cargar_modelos():
+    global scaler, kmeans, umap_model, feature_cols
+
+    if scaler is None:
+        scaler = joblib.load(
+            hf_hub_download(repo_id, "modelos/scaler.pkl")
+        )
+
+    if kmeans is None:
+        kmeans = joblib.load(
+            hf_hub_download(repo_id, "modelos/kmeans_umap.pkl")
+        )
+
+    if umap_model is None:
+        umap_model = joblib.load(
+            hf_hub_download(repo_id, "modelos/umap_model.pkl")
+        )
+
+    if feature_cols is None:
+        feature_cols = joblib.load(
+            hf_hub_download(repo_id, "modelos/feature_cols.pkl")
+        )
+
+    return scaler, kmeans, umap_model, feature_cols
