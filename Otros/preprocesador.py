756f55cfd5567bc4e33bd126b11ce854d61f5133
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-12 09:55:00 +0200

    Update preprocesador.py

--- Otros/preprocesador.py
+++ Otros/preprocesador.py
@@ -1,13 +1,9 @@
 import numpy as np
-from Otros.cargar_modelos import scaler, umap_model, kmeans, feature_cols
-
+from Otros.cargar_modelos import scaler, kmeans, feature_cols
 def preprocesar_features(features_dict):
 
-    dtype = kmeans.cluster_centers_.dtype  
-    x = np.array([[features_dict[col] for col in feature_cols]], dtype=dtype)
-    x_scaled = scaler.transform(x.astype(dtype, copy=False))
-    x_umap = umap_model.transform(x_scaled)
-    x_umap = x_umap.astype(dtype, copy=False)
+    x = np.array([[features_dict[col] for col in feature_cols]], dtype=np.float32)
 
-    return x_umap
-
+    x_scaled = scaler.transform(x)
+    
+    return x_scaled
