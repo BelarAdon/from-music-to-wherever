7cc8d09c5b9af5fb976a22c93dcf1483b34576fe
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-15 11:03:16 +0200

    Update mood_imagenes.py

--- Otros/mood_imagenes.py
+++ Otros/mood_imagenes.py
@@ -1,9 +1,9 @@
 mood_imagenes = {
-    "Concierto / Euforia": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/euforia.jpg",
-    "Intenso / Dramático": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/dramatic.jpg",
-    "Instrumental / Concentración": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/focus.jpg",
-    "Chill groove / Urbano suave": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/chill.jpg",
-    "Happy / Buen rollo": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/happy.jpg",
-    "Fiesta / Subidón": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/party_.jpg",
-    "Triste / Melancólico": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/blob/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/sad.jpg"
+    "Concierto / Euforia": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/euforia.jpg",
+    "Intenso / Dramático": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/dramatic.jpg",
+    "Instrumental / Concentración": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/focus.jpg",
+    "Chill groove / Urbano suave": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/chill.jpg",
+    "Happy / Buen rollo": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/happy.jpg",
+    "Fiesta / Subidón": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/party_.jpg",
+    "Triste / Melancólico": "https://raw.githubusercontent.com/marinaxiuping/from-music-to-wherever/14b861ad2d4fbe46a7daf007f385f07f1ae1e8d0/imagenes/sad.jpg"
 }
