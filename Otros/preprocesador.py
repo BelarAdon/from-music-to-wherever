432f13d7ff8300bc86ac41fd6e971906713d4577
Marina Xiuping <marinaxiuping@gmail.com>
2026-06-15 11:21:52 +0200

    Update preprocesador.py

--- Otros/preprocesador.py
+++ Otros/preprocesador.py
@@ -1,10 +1,6 @@
 import numpy as np
 
+import numpy as np
 
-def preprocesar_features(features_dict: dict, feature_cols: list, scaler) -> np.ndarray:
-
-    x = np.array(
-        [[features_dict[col] for col in feature_cols]],
-        dtype=np.float32
-    )
-    return scaler.transform(x)
+def preprocesar_features(fila, umap_cols):
+    return np.array([[fila[col] for col in umap_cols]], dtype=np.float32)
